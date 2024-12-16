556bbf08e437a7853b8b0807bbe3f074077585dc
shivam <shivamgiri0406@gmail.com>
2024-12-16 08:27:50 +0530

    This is an Project Management App

--- lib/screens/productivity_screen.dart
+++ lib/screens/productivity_screen.dart
@@ -45,7 +45,7 @@ class ProductivityScreen extends StatelessWidget {
                   ),
                 ),
               ),
-
+              
               // Bar Chart Section
               const ProductivityGraph(),
               // Subtasks Section
@@ -58,75 +58,6 @@ class ProductivityScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildBar(double height, String day, {bool isHighlighted = false}) {
-    return Column(
-      children: [
-        Container(
-          width: 24.0,
-          height: height,
-          decoration: BoxDecoration(
-            color: isHighlighted ? Colors.grey : const Color(0xFF5DBB9F),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        const SizedBox(height: 4.0),
-        Text(
-          day,
-          style: const TextStyle(color: Colors.grey, fontSize: 12.0),
-        ),
-      ],
-    );
-  }
 
-  Widget _buildLegend(String text, Color color) {
-    return Row(
-      children: [
-        CircleAvatar(radius: 6, backgroundColor: color),
-        const SizedBox(width: 4),
-        Text(
-          text,
-          style: const TextStyle(fontSize: 12, color: Colors.grey),
-        ),
-      ],
-    );
-  }
 
-  Widget _buildSubtask(String title, bool isChecked) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Icon(
-              isChecked ? Icons.check_circle : Icons.radio_button_unchecked,
-              color: const Color(0xFF5DBB9F),
-            ),
-            const SizedBox(width: 8.0),
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Color(0xFF2B184D),
-              ),
-            ),
-          ],
-        ),
-        const Row(
-          children: [
-            CircleAvatar(
-              radius: 12,
-              backgroundColor: AppColors.subtitleColor,
-              child: Icon(Icons.person, color: AppColors.buttonTextColor, size: 12),
-            ),
-            SizedBox(width: 4),
-            CircleAvatar(
-              radius: 12,
-              backgroundColor: Colors.grey,
-              child: Icon(Icons.person, color: AppColors.buttonTextColor, size: 12),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
 }
